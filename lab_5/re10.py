81d8ef1433125b4e9974ffd53285bdf850de860c
Jateq <temirlan.eraly1@gmail.com>
2022-03-13 22:00:10 +0600

    lab5 10

--- lab_5/re10.py
+++ lab_5/re10.py
@@ -1,6 +1,24 @@
 import re
-with open('row.txt', 'r', encoding= 'utf-8') as file:
-    text = file.read()
-pattern = r'(?<!^)(?=[A-Z])'
-name = re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
-print(name)
+# with open('row.txt', 'r', encoding= 'utf-8') as file:
+#     text = file.read()
+# pattern = r'(?<!^)(?=[A-Z])'
+# name = re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
+# print(name)
+
+
+ 
+def change_case(str):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+     
+str = "alikhanErlanuly"
+print(change_case(str))
+
+import re
+
+# text = 'CamelCaseLol'
+# output = camel_case_lol
+# f = re.sub(r'(?!^)(?=[A-Z][a-z]+)', '_', text).lower()
+
+
+# print(f)
