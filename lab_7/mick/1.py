56de3fd99c541ab017dc9c173720350189b4b80c
Jateq <temirlan.eraly1@gmail.com>
2022-04-01 21:45:04 +0600

    lab 7 finished

--- lab_7/mick/1.py
+++ lab_7/mick/1.py
@@ -1,80 +1,67 @@
-import pygame, datetime
-from math import sin,cos,pi
+import pygame as pg
+import datetime
+# -----------------------------------------------------------------
+pg.init()
+# Initializing-----------------------------------------------------
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+clock = pg.time.Clock()
+# Screen ----------------------------------------------------------
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption('Clock')
+# Colors ----------------------------------------------------------
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+# Background -----------------------------------------------------
+background = pg.transform.scale(pg.image.load('img/Main.jpeg'), (WIDTH, HEIGHT))
 
-size = 800
-center = (size/2,size/2)
-clockRadius = 400
-digits = {1: 'I', 2:'II',3:'III',4:'IV', 5:'V',6:'VI',7:'VII',8:'VII',9:'IX',10:'X',11:'XI',12:'XII'}
-pygame.init()
-screen = pygame.display.set_mode([size,size])
-# rename
-pygame.display.set_caption('Mickey Mouse Clock')
-clock = pygame.time.Clock()
 
-#import img from folder img
-mickey = pygame.transform.scale(pygame.image.load('img\\mickey.png'), (int(574 * 1.25), int(485 * 1.25)))
-right = pygame.transform.scale(pygame.image.load('img\\right.png'), (594 // 2, 322 // 2))
-left = pygame.transform.scale(pygame.image.load('img\\left.png'), (770 // 2, 230 // 2))
+# Font ----------------------------------------------------------
+font = pg.font.SysFont("Times New Roman", 30)
 
-# functions for clock
-#convert numbers to roman numbers
-def numbers(number, number_size, position):
-    font = pygame.font.SysFont('Arial', number_size, bold=True)
-    text = font.render(number, True, pygame.Color('white'))
-    text_rect = text.get_rect(center=(position))
-    screen.blit(text, text_rect)
-# 
-def polar_to_cartesian(r, theta):
-    x, y = r * sin(pi * theta / 180), r * cos(pi * theta / 180)
-    return x + size / 2, -(y - size / 2)
-# rotate img
-def blitRotate(surf, image, pos, originPos, angle):
-    image_rect = image.get_rect(topleft = (pos[0]- originPos[0], pos[1] - originPos[1]))
-    offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
-    rotated_offset = offset_center_to_pivot.rotate(angle)
-    rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
-    rotated_image = pygame.transform.rotate(image, -angle)
-    rotated_image_rect = rotated_image.get_rect(center = rotated_image_center)
-    surf.blit(rotated_image, rotated_image_rect)
+# Images -------------------------------------------------------------
+right = pg.image.load('./img/left.png')
+left = pg.image.load('./img/right.png')
+# Changing resolution
+# 1400//1,75 = 800 and 1050//1,75 = 600 then 1450x1050 => 800x600
+rhand = pg.transform.scale(pg.image.load('img/left.png'), (right.get_width()//1.75, right.get_height()//1.75))
+lhand = pg.transform.scale(pg.image.load('img/right.png'), (left.get_width()//1.75, left.get_height()//1.75))
 
-while True:
-    # draw
-    screen.fill(pygame.Color('white'))
-    pygame.draw.circle(screen, pygame.Color('black'), center, clockRadius-10,10) # draw black circle outside
-    pygame.draw.circle(screen, pygame.Color('gray'), center, clockRadius-20)#draw gray circle inside
-    pygame.draw.circle(screen, pygame.Color('black'), center, 10)#draw center dot   
+# Rotate functions ------------------------------------------------------
 
-    # roman numbers on the background
-    [numbers(digits[i], 80, polar_to_cartesian(clockRadius - 70, i * 30)) for i in range(1,13)]
 
-    # draw the second indicators on the clock
-    for i in range(0,360,6):
-        if i % 5 != 0:
-            pygame.draw.line(screen, pygame.Color('black'), polar_to_cartesian(clockRadius - 15, i), polar_to_cartesian(clockRadius-30,i),2)
-        else:
-             pygame.draw.line(screen, pygame.Color('black'), polar_to_cartesian(clockRadius - 15, i), polar_to_cartesian(clockRadius-30,i),6)
+def blit_rotate_center(image, x0, y0, angel):  # topleft x0 and y0 coordinates of the image
+    rotated_image = pg.transform.rotate(image, angel)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=(x0, y0)).center)
+    screen.blit(rotated_image, new_rect)
 
-    # mickey
-    screen.blit(mickey,(40,70))
-    # time
-    current_time = datetime.datetime.now()
-    second = current_time.second
-    minute = current_time.minute
 
-    # second
-    theta = second * (360 / 60)
-    blitRotate(screen, left, center, (left.get_width() / 2 - 145, left.get_height() / 2), theta - 87)
+running = True
+while running:
+    clock.tick(FPS)
+    screen.blit(background, (0, 0))
+    # Buttons --------------------------------------------------------
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    # Time -----------------------------------------------------------
+    time = datetime.datetime.now()
+    hour = time.hour
+    minute = time.minute
+    second = time.second
+    # Background -----------------------------------------------------
+    screen.blit(background, (0, 0))
+    text = font.render(f'The time is now: {hour}:{minute}:{second}', True, BLUE)
+    blit_rotate_center(rhand, WIDTH // 2 - rhand.get_width()//2, HEIGHT // 2 - rhand.get_height()//2,
+                       (-6*minute) - 11)  # Error 11
+    blit_rotate_center(lhand, WIDTH // 2 - lhand.get_width() // 2, HEIGHT // 2 - lhand.get_height() // 2,
+                       (-6*second) - 3)  # Error 3
+    screen.blit(text, (250, 550))
 
-    # minute
-    theta = (minute + second / 60) * (360 / 60)
-    blitRotate(screen, right, center, (right.get_width() / 2 + 110, right.get_height() / 2 + 75), theta + 75)
-
-    # display
-    pygame.display.flip()
-    clock.tick(60)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-
-pygame.quit()
+    pg.display.update()
+pg.quit()
+exit()
